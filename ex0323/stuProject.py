088cdaf480524f5f9858b972963bc195130223f9
onulee <onulee@naver.com>
2022-03-23 14:08:23 +0900

    commit

--- ex0323/stuProject.py
+++ ex0323/stuProject.py
@@ -5,66 +5,30 @@
 from stu_def import *
 import json
 
-stuSave = jsonRead()    # 학생리스트 읽기 호출
-sCount = stuCount()     # 학생번호 증가함수 호출
+# stuSave = jsonRead()    # 학생리스트 읽기 호출
+# sCount = stuCount()     # 학생번호 증가함수 호출
 while True:
     choice=0  #화면출력 선택번호변수
     choice = screen_print()  # 화면출력함수 호출
     
     if choice==1: # 학생성적입력
-        # 학생성적입력함수 호출 (sCount,stuSave)
+        # 학생성적입력함수 호출 
         sCount = stu_input()
     elif choice==2:
-        # 학생성적 수정 (stuSave)
+        # 학생성적 수정 호출
         stu_modify()
         
     elif choice==3:
-        print('학생성적 삭제를 선택하셨습니다.')
-        searchName = input('삭제할 이름을 입력하세요.>>')
-        count=0
-        for i,stu in enumerate(stuSave):
-            if searchName in stu.values():
-                del(stuSave[i])
-                print('{} 학생이 삭제되었습니다.'.format(searchName))
-                count=1
-                break
-        if count==0:
-            print('{} 학생이 없습니다.'.format(searchName)) 
+        stu_delete() #학생성적 삭제호출
         
     elif choice==4:
-        print('번호','이름','국어','영어','합계','평균','등수',sep='\t')  
-        print('-'*60)
-        # [[1,홍길동,100,100,200,100.0,0]]
-        for stu in stuSave:
-            # print정렬
-            print(stu['stuno'],stu['stuname'],stu['kor'],stu['eng'],\
-                stu['total'],stu['avg'],stu['rank'],sep='\t')
-            # for k,v in stu.items():
-            #    print('{}\t'.format(v),end='') 
-            # print() #줄바꿈
+        stu_print()  # 학생성적 전체출력 호출
+        
     elif choice==5:
-        searchName = input('출력할 학생이름을 입력하세요.>>')
-        count=0
-        for i,stu in enumerate(stuSave):
-            if searchName in stu.values():
-                print('번호','이름','국어','영어','합계','평균','등수',sep='\t')  
-                print('-'*60)
-                print(stu['stuno'],stu['stuname'],stu['kor'],stu['eng'],\
-                stu['total'],stu['avg'],stu['rank'],sep='\t')
-                count=1
-                break
-        if count==0:
-            print('{} 학생이 없습니다.'.format(searchName)) 
+        stu_search() # 학생검색 출력 호출
     
     elif choice==6:
-        for stu in stuSave:
-            rcount=1
-            for stu2 in stuSave:
-                if stu['total'] < stu2['total']: #total점수 비교
-                    rcount += 1    # stu2가 더 클경우 1증가
-            stu['rank'] = rcount   # 등수입력        
-        
-        print('등수처리가 완료되었습니다.')        
+        stu_rank()   # 학생등수처리 호출
                 
     elif choice==0:
         print('프로그램을 종료합니다.')
