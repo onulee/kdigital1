094b58b8f1255ede522b5c4f9be263cfc460b255
onulee <onulee@naver.com>
2022-03-25 11:52:40 +0900

    commit

--- ex0325/main.py
+++ ex0325/main.py
@@ -1,12 +1,7 @@
 # 학생관리폴더 - 학번,이름,전화번호,주소,성별,학년,학과
 # 학생성적폴더 - 학번,국어,영어,수학,합계,평균,등수
-
 from stumanage.student import Student
 
-
-s = Student(1,'홍길동',100,100)
-
-s.setKor(-100)
-s.kor=-100
-
-print('{},{}'.format(s.stuname,s.kor))
+s1 = Student(1,'홍길동',100,100)
+s2 = Student(2,'이순신',100,90)
+s3 = Student(1,'홍길동',90,90)
--- ex0325/stumanage/student.py
+++ ex0325/stumanage/student.py
@@ -10,14 +10,22 @@ class Student:
     def __init__(self,stuno,stuname,kor,eng):
         self.stuno = stuno
         self.stuname = stuname
-        self.__kor = kor   # private변수선언
+        # self.__kor = kor   # private변수선언-같은클래스 내에서만 입력가능
+        self.kor = kor   
         self.eng = eng
         self.total = kor+eng
         self.avg = self.total/2
         
+    def __eq__(self,other):
+        return self.stuname == other.stuname and \
+            self.stuno == other.stuno   
+    
+    def getKor(self):
+        return self.__kor
+        
     def setKor(self,kor):
         if kor>=0:
-            self.kor=kor
+            self.__kor=kor
         else:
             print('입력이 잘못되었습니다.')
             # try:
