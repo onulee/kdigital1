6bafb183b00e72681660268c61b54e52377bb82e
onulee <onulee@naver.com>
2022-04-19 14:21:03 +0900

    commit

--- web0419/w0419_04.py
+++ web0419/w0419_04.py
@@ -4,17 +4,28 @@ url="http://www.google.com"
 res = requests.get(url) #url 파일정보 가져오기
 res.raise_for_status()  # 200코드 확인
 
-
 p=re.compile("ca.e")
-# temp = input("글자를 입력하세요.>>")
-# match:정확히 일치해야지만 찾아짐. search : 단어 내에 포함되어 있으면 찾아짐.
-m=p.findall("good morning good care")
-print(m)
-
-if len(m)==0:
-    print("매칭되는 단어가 없습니다.")
+temp = input("글자를 입력하세요.>>")
+m=p.search(temp)  # match,search,findall
+if m:
+    print("매칭 단어 : ",m.group())  #complie 정규표현식에 맞는 부분만 출력
+    print("매칭 단어 : ",m.string)  # 정규표현식이 맞으면 입력된 문자를 전체출력
+    print("매칭 단어 : ",m.start()) # 맞으면 시작위치
+    print("매칭 단어 : ",m.end())   # 맞으면 끝나는 위치
+    print("매칭 단어 : ",m.span())  # 맞으면 시작,끝나는 위치 알려줌.
 else:
-    print("매칭되는 단어가 있습니다.")    
+    print("매칭되는 단어가 아닙니다.")
+
+# p=re.compile("ca.e")
+# # temp = input("글자를 입력하세요.>>")
+# # match:정확히 일치해야지만 찾아짐. search : 단어 내에 포함되어 있으면 찾아짐.
+# m=p.findall("good morning good care")
+# print(m)
+
+# if len(m)==0:
+#     print("매칭되는 단어가 없습니다.")
+# else:
+#     print("매칭되는 단어가 있습니다.")    
 
 
 # if m:
