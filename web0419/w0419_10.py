7451e1be3cb4f02e47ebd63711c8f050f836e31b
onulee <onulee@naver.com>
2022-04-19 18:17:48 +0900

    commit

--- web0419/w0419_10.py
+++ web0419/w0419_10.py
@@ -1,2 +1,10 @@
 # 평균평점
-rate = (float(9.98)+float(9.97))/2
+# rate = (float(9.98)+float(9.97))/2
+import requests
+from bs4 import BeautifulSoup
+headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"}
+url="https://comic.naver.com/webtoon/weekday"
+res = requests.get(url,headers=headers)
+soup = BeautifulSoup(res.text,"lxml")
+
+cartoons = soup.find_all("td",{"class":"title"})
