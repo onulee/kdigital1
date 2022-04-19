93f9845f3cc734e3a06fc5f123829774e97a70db
onulee <onulee@naver.com>
2022-04-19 11:45:07 +0900

    commit

--- web0419/w0419_03.py
+++ web0419/w0419_03.py
@@ -1,7 +1,8 @@
 import requests
+headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"}
 # res = requests.get("http://www.google.com")
-res = requests.get("https://www.whatismybrowser.com/detect/what-is-my-user-agent/")
+res = requests.get("http://www.melon.com",headers=headers)
 res.raise_for_status()
-with open("detected_value.html","w","utf-8") as f:
+with open("melon.html","w",encoding="utf-8") as f:
     f.write(res.text)
 print(res.text)
