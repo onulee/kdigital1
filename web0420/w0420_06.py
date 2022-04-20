c7d95ed39b38e4ec53d4d72b4c34c5f0d8ffb466
onulee <onulee@naver.com>
2022-04-20 18:06:10 +0900

    commit

--- web0420/w0420_06.py
+++ web0420/w0420_06.py
@@ -11,13 +11,38 @@ soup = BeautifulSoup(res.text,"lxml")
 # 정규표현식
 # items = soup.find_all("li",{"class":re.compile("^list_4")})
 items = soup.find_all("li",{"class":"list_4"})
-name = items[0].find("div",{"class":"name"}).strong.get_text()
-name = name.strip()
-print(len(items))
-print("숙소명 : ",name)
+count=1
+for i,item in enumerate(items):
+    item_url = item.find("a")["href"]
+    name = item.find("img",{"class":"lazy"})["alt"]
+    
+    if item.find("div",{"class":"name"}).find("div",{"class":"badge"}):
+        name2 = item.find("div",{"class":"name"}).find("div",{"class":"badge"}).next_sibling
+    else:
+        name2 = item.find("div",{"class":"name"}).find("strong").get_text()
+            
+    name2 = name2.strip()
+    rate = item.find("p",{"class":"score"}).span.em.get_text()
+    rate = float(rate)
+    # span태그 다음요소 선택, 좌우공백제거
+    rate_cnt = item.find("p",{"class":"score"}).span.next_sibling.strip()
+    # (184) -> 첫번째자리에서 마지막앞자리까지 슬라이싱 184
+    rate_cnt = int(rate_cnt[1:-1])
+    
+    if rate<9.0 or rate_cnt<100:
+        continue
+    
+    print("{}. 숙소명 : {}".format(count,name))
+    print("{}. 숙소명2 : {}".format(count,name2))
+    print("평점 : ",rate)
+    print("상품평:",rate_cnt)
+    print("바로가기 링크 : ",item_url)
+    print("-"*20)
+    count += 1
+    
+    
 
 
 
 
-# name = items[2].find("img",{"class":"lazy"})["alt"]
 
