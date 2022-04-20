61cb4041fb0e287c103845df18edd045fee9bcf8
onulee <onulee@naver.com>
2022-04-20 10:48:59 +0900

    commit

--- web0419/w0419_10.py
+++ web0419/w0419_10.py
@@ -3,25 +3,29 @@
 import requests
 from bs4 import BeautifulSoup
 headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"}
-url="https://comic.naver.com/webtoon/list?titleId=748105&weekday=thu"
-res = requests.get(url,headers=headers)
-soup = BeautifulSoup(res.text,"lxml")
-
 all_rate = 0  #전체 평균변수
-web_table = soup.find("table",{"class":"viewList"})
-cartoons = web_table.find_all("tr")
-for i,cartoon in enumerate(cartoons):
-    car_title = cartoon.find("td",{"class":"title"})
-    if not car_title:  #글자가 없을때 false로 인식
-        continue
-    car_rate = cartoon.find("div",{"class":"rating_type"})
-    temp_rate = car_rate.strong.get_text()
-    all_rate += float(temp_rate)
-    car_date = cartoon.find("td",{"class":"num"})
-    print("제목 : ",car_title.a.get_text())   # 제목
-    print("평점 : ",temp_rate)   # 평점
-    print("날짜 : ",car_date.get_text())   # 날짜
-    print("-"*20)
+
+# 전체 페이지 for문
+page_count=5
+for i in range(1,page_count+1):
+    url="https://comic.naver.com/webtoon/list?titleId=748105&weekday=thu&page="+str(i)
+    res = requests.get(url,headers=headers)
+    soup = BeautifulSoup(res.text,"lxml")
     
-print("전체평균 : {:.2f}".format(all_rate/(len(cartoons)-1)))    
+    web_table = soup.find("table",{"class":"viewList"})
+    cartoons = web_table.find_all("tr")
+    for i,cartoon in enumerate(cartoons):
+        car_title = cartoon.find("td",{"class":"title"})
+        if not car_title:  #글자가 없을때 false로 인식
+            continue
+        car_rate = cartoon.find("div",{"class":"rating_type"})
+        temp_rate = car_rate.strong.get_text()
+        all_rate += float(temp_rate)
+        car_date = cartoon.find("td",{"class":"num"})
+        print("제목 : ",car_title.a.get_text())   # 제목
+        print("평점 : ",temp_rate)   # 평점
+        print("날짜 : ",car_date.get_text())   # 날짜
+        print("-"*20)
+    
+print("전체평균 : {:.2f}".format(all_rate/((len(cartoons)-1)*page_count)))    
     
