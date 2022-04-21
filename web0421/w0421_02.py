7d336429842a7fa4777a6c7d13d9c4a814b7f03c
onulee <onulee@naver.com>
2022-04-21 14:21:13 +0900

    commit

--- web0421/w0421_02.py
+++ web0421/w0421_02.py
@@ -8,26 +8,67 @@ headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537
 res = requests.get(url,headers=headers)
 soup = BeautifulSoup(res.text,"lxml")
 
-images = soup.find_all("img",{"class":"thumb_img"})
-for i,image in enumerate(images):
-    img_url = image["src"]
-    # startswith 함수 : 해당문자로 시작하는지 확인
-    if img_url.startswith("//"):
-        img_url = "https:"+img_url
-        
-    print(img_url)
+temp_ol = soup.find("ol",{"class":"type_plural list_exact movie_list"})
+# 역대 관객순위 30위
+screens = temp_ol.find_all("li")
+for i,screen in enumerate(screens):
+    s_cnt = screen.find_all("dd",{"class":"cont"})
+    # print(len(s_cnt))
+    # s_cnt 마지막list를 출력
+    print(s_cnt[len(s_cnt)-1].get_text())
     
-    # 이미지 링크를 가지고 requests함수 실행
-    img_res = requests.get(img_url)
-    img_res.raise_for_status()
     
-    # with open("aaa.html","w",encoding="utf-8") 
-    # 문자저장시 인코딩이 필요
+    # 이미지링크, url주소가 https: 없으면 추가
+    s_img = screen.find("img",{"class":"thumb_img"})["src"]
+    if s_img.startswith("//"):
+        s_img = "https:"+s_img
+    # 영화제목    
+    s_title = screen.find("a",{"class":"tit_main"}).get_text()
+    print(s_title)
+    # 평점출력 -> float형변환
+    rate = float(screen.find("em",{"class":"rate"}).get_text())
+    print(rate)
+    
+    
+    # 이미지저장
+    s_img_res = requests.get(s_img)
+    s_img_res.raise_for_status() #데이터 없을시 종료
+    
     with open("movie2021_{}.jpg".format(i+1),"wb") as f:
-        # requests에서 리턴하는 3가지 - status_code-상태,text-글자,content-파일
-        # f.write(res.text)
-        f.write(img_res.content)    
+        # requests -> res 리턴 3가지 : status_code,text,content
+        f.write(s_img_res.content)
+    
     
     # 상위 5개 이미지만 출력    
     if i>=4:
-        break;       
+        break; 
+
+# images = soup.find_all("img",{"class":"thumb_img"})
+# for i,image in enumerate(images):
+#     img_url = image["src"]
+#     # startswith 함수 : 해당문자로 시작하는지 확인
+#     if img_url.startswith("//"):
+#         img_url = "https:"+img_url
+    
+#     rate = image.find("dd",{"class":"score"})
+#     print(rate)
+#     # 평점
+#     print(rate) 
+#     # 이미지 url링크   
+#     # print(img_url)
+    
+#     # 이미지 링크를 가지고 requests함수 실행
+#     img_res = requests.get(img_url)
+#     img_res.raise_for_status()
+    
+#     # with open("aaa.html","w",encoding="utf-8") 
+#     # 문자저장시 인코딩이 필요
+#     with open("movie2021_{}.jpg".format(i+1),"wb") as f:
+#         # requests에서 리턴하는 3가지 - status_code-상태,text-글자,content-파일
+#         # f.write(res.text)
+#         # f.write(img_res.content) 
+#         pass   
+    
+    # 상위 5개 이미지만 출력    
+    # if i>=4:
+    #     break;       
