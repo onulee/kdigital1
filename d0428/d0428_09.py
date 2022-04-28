e3d69ce8aba9d4a315003268ed2bb1005cbaa0a3
onulee <onulee@naver.com>
2022-04-28 14:25:31 +0900

    commit

---
+++ d0428/d0428_09.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+# excel파일 불러오기
+# df=pd.read_excel('score.xlsx',index_col='지원번호')
+# print(df)
+
+# 숫자컬럼 최대값, 평균, 숫자 분포도 통계를 보여줌.
+# print(df.describe())
+
+# 컬럼의 타입
+# print(df.info())
+
+
+df=pd.read_excel('user.xlsx')
+print(df)
+# 컬럼 타입정보 - object,float,int
+# print(df.info())
+# 상위 5개만 보여짐. 10라는 숫자를 넣으면 10개 보여짐.
+# print(df.head())
+# print(df.head(10))
+
+# 하위 5개만 보여짐
+# print(df.tail())
+# print(df.tail(20))
+
+# 배열구조로 출력
+print(df.values)
+# 1개 row출력
+print(df.iloc[0])
+# 1개 col출력
+print(df['first_name'])
+# 2개 col출력
+print(df[['first_name','gender']])
+# index 출력
+print(df.index)
+# 컬럼 출력
+print(df.columns)
+# row,column수를 출력
+print(df.shape)
+
+# skiprows : 개수만큼 제외, nrows : 개수만큼 가져옴
+# df = pd.read_excel('user.xlsx',skiprows=[i for i in range(0,500)],nrows=10) 
+# print(df)
