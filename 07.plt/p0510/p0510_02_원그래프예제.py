4f847336f319148568717caa8995c4abb742f78f
onulee <onulee@naver.com>
2022-05-10 14:20:49 +0900

    commit

--- 07.plt/p0510/p0510_02_원그래프예제.py
+++ 07.plt/p0510/p0510_02_원그래프예제.py
@@ -4,7 +4,6 @@
 
 import matplotlib.pyplot as plt
 import matplotlib
-from pyparsing import col
 matplotlib.rcParams['font.family'] = 'Malgun Gothic'
 # matplotlib.rcParams['font.family'] = 'AppleGothic'
 matplotlib.rcParams['font.size'] = 15
@@ -21,9 +20,14 @@ df_2 = df[:5].sort_values('국어',ascending=False)
 values = df_2['국어']
 labels =df_2['이름']
 colors =['#ffebb5','#ffc0b5','#dbffb5','#b5f9ff','#b5b9ff']
+wedgeprops = {'width':0.6,'edgecolor':'w','linewidth':2}
+def auto_pct(pct):
+    return '{:.1f}%'.format(pct) if pct>10 else ''
 plt.figure(figsize=(8,5))
 explode = [0,0,0,0,0]
-plt.pie(values,labels=labels,colors=colors, explode=explode,autopct='%.1f%%',startangle=90,counterclock=False)
+plt.pie(values,labels=labels,colors=colors, \
+    wedgeprops=wedgeprops,explode=explode,autopct=auto_pct,\
+        pctdistance=0.7,startangle=90,counterclock=False)
 plt.title('국어성적 그래프')
 # 범례넣기
 plt.legend(loc=(1.1,0.3))
