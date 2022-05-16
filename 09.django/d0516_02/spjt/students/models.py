809693fd2f0041d623fc841bf6c5f3bf0165d5e4
onulee <onulee@naver.com>
2022-05-16 18:06:56 +0900

    commit

--- 09.django/d0516_02/spjt/students/models.py
+++ 09.django/d0516_02/spjt/students/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Student클래스 선언
+class Student(models.Model):
+    # s_name varchar2(100) 
+    s_name = models.CharField(max_length=100) # 문자(varchar2)
+    s_major = models.CharField(max_length=100)
+    s_age = models.IntegerField(default=0)   # 정수
+    s_grade = models.IntegerField(default=0) # 정수
+    s_gender = models.CharField(max_length=3) # 문자
+    s_date = models.DateField(auto_now=True)  # 날짜
+    
+    def __str__(self):
+        return self.s_name
