f6c16968af2f5df75ef7291500cd8c70fc985d0f
onulee <onulee@naver.com>
2022-05-18 12:23:11 +0900

    commit

--- 09.django/d0518/spjt/students/views.py
+++ 09.django/d0518/spjt/students/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 from students.models import Student
 
@@ -13,6 +15,17 @@ def stuWriteOk(request):
     s_age = request.POST.get('age')
     s_grade = request.POST.get('grade')
     s_gender = request.POST.get('gender')
-    s_hobby = request.POST.getlist('hobby')  # hobby배열형태
+    s_hobby = request.POST.getlist('hobby')  # hobby list배열형태
+    print("s_hobby list : ",s_hobby)
+    # db -> list타입 저장안됨.
+    # s_hobby list타입 -> str타입변경
+    # s_hobby list :  ['게임', '골프', '수영', '독서']
+    s_hobby = ','.join(s_hobby)
+    print("s_hobby join : ",s_hobby)
+    print("s_hobby join타입 : ",type(s_hobby))
+    ss_hobby = s_hobby.split(',')
+    print('ss_hobby :',ss_hobby)
+    print('ss_hobby 타입 :',type(ss_hobby))
     
-    return 
+    
+    return HttpResponseRedirect(reverse('index'))
