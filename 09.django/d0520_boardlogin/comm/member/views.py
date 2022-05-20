6ff088da6bc7b788f4b537b2a1798d3c71706081
onulee <onulee@naver.com>
2022-05-20 18:14:27 +0900

    commit

--- 09.django/d0520_boardlogin/comm/member/views.py
+++ 09.django/d0520_boardlogin/comm/member/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from member.models import Member
 
 # logout페이지 함수
@@ -19,16 +19,22 @@ def login(request):
         # db에서 id,pw로 검색
         try:
             qs = Member.objects.get(id=id,pw=pw)
-        except Member.DoesNotExist:
-            qs = None
-        
-        # 데이터가 있는지 확인
-        if qs:
             request.session['session_id'] = qs.id
             request.session['session_name'] = qs.name
             request.session['session_nickname'] = qs.nickname
-            return redirect('/')       
-            # return render(request,'/index.html',{'msg':'로그인 성공'})       
-        else:
+            return redirect('/') 
+        except Member.DoesNotExist:
+            qs = None
             context={'msg':'아이디 또는 패스워드가 일치하지 않습니다. 다시 로그인 바랍니다.'}
             return render(request,'login.html',context)
+        
+        # 데이터가 있는지 확인
+        # if qs:
+        #     request.session['session_id'] = qs.id
+        #     request.session['session_name'] = qs.name
+        #     request.session['session_nickname'] = qs.nickname
+        #     return redirect('/')       
+        #     # return render(request,'/index.html',{'msg':'로그인 성공'})       
+        # else:
+        #     context={'msg':'아이디 또는 패스워드가 일치하지 않습니다. 다시 로그인 바랍니다.'}
+        #     return render(request,'login.html',context)
