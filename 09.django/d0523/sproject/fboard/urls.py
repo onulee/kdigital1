791065285b189aae3460af6b594f5086658b2c5b
onulee <onulee@naver.com>
2022-05-23 16:14:38 +0900

    commit

--- 09.django/d0523/sproject/fboard/urls.py
+++ 09.django/d0523/sproject/fboard/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('fList/',views.fList,name='fList'),
     path('fWrite/',views.fWrite,name='fWrite'),
     path('<str:f_no>/fView',views.fView,name='fView'),
+    # 답글쓰기
+    path('<str:f_no>/fReply',views.fReply,name='fReply'),
 ]
--- 09.django/d0523/sproject/fboard/views.py
+++ 09.django/d0523/sproject/fboard/views.py
@@ -1,6 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from fboard.models import Fboard
 from member.models import Member
+from django.db.models import F 
+
+# 게시판 답글쓰기 함수
+def fReply(request,f_no):
+    if request.method == 'GET':
+        qs = Fboard.objects.get(f_no=f_no) 
+        context={'board':qs}
+        return render(request,'fReply.html',context)
+    else:
+        # id = request.session.session_id
+        id = request.POST.get('id')
+        print("id:",id)
+        member = Member.objects.get(id=id)
+        # request 넘어온 데이터타입 : str타입
+        group = int(request.POST.get('group'))
+        step = int(request.POST.get('step'))
+        indent = int(request.POST.get('indent'))
+        
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        file = request.POST.get('file',None)
+        
+        # 부모group에서 부모보다 큰 step 1씩증가, gt보다 큰수
+        # reboard = Fboard.objects.filter(f_group=group,f_step_gt=step)
+        # # F참조객체: db에서 검색을 해서 가져올수 있음.
+        # reboard.update(f_step=F('f_step')+1)
+        
+        # 부모group에서 부모보다 큰 step 1씩증가, gt보다 큰수
+        # F참조객체: db에서 검색을 해서 가져올수 있음.
+        Fboard.objects.filter(f_group=group,f_step__gt=step).update(f_step=F('f_step')+1)
+        
+        # step: 출력순서, indent: 들여쓰기
+        qs=Fboard(member=member,f_title=title,f_content=content,f_group=group\
+            ,f_step=step+1,f_indent=indent+1,f_file=file)
+        qs.save() # f_no
+        
+        return redirect('fboard:fList')
+
+    
 
 # 게시판 읽기 함수
 def fView(request,f_no):
@@ -13,13 +52,20 @@ def fWrite(request):
     if request.method == 'GET':
         return render(request,'fWrite.html')
     else:
+        # form넘어온 데이터
         id = request.POST.get("id")
         member = Member.objects.get(id=id)
         title = request.POST.get("title")
         content = request.POST.get("content")
-        file = request.POST.get("f_file",None)
+        file = request.POST.get("file",None)
+        # db 저장
+        qs = Fboard(member=member,f_title=title,f_content=content,f_file=file)
+        qs.save()
+        qs.f_group = qs.f_no
+        qs.save()
+        return redirect('fboard:fList')
         
-        qs = Fboard()
+        
 
 # 게시판 리스트 함수
 def fList(request):
